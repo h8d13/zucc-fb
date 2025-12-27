28594624625ebe8f32ac93010398b52958acf46d
h8d13 <hadrien.loge@gmail.com>
2025-12-27 23:05:14 +0100

    oopsies

--- xtraktor.sh
+++ xtraktor.sh
@@ -6,7 +6,7 @@ has_libs=$(pacman -Q | grep -i "noto")
 echo "$has_libs" | grep -q "noto-fonts" || { echo "Missing required noto packages"; exit 1; }
 echo "$has_libs" | grep -q "noto-fonts-cjk" && echo "$has_libs" | grep -q "noto-fonts-extra"  || { echo "Missing required noto packages"; exit 1; }
 
-f_path = "usr/share/fonts/noto"
+f_path="usr/share/fonts/noto"
 
 tar -czf in/noto-fonts-bundle.tar.gz -C / \
     "$f_path/NotoSans-Regular.ttf" \
